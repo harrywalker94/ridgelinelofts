12a6b26f03a1d457909565cac4e72c9e0fa5478b
harry walker <harrywalker312@gmail.com>
2020-11-23 18:58:34 +0000

    djhihi

--- js/scripts.js
+++ js/scripts.js
@@ -31,6 +31,24 @@ function scrollFunction() {
   }
 }
 
+$('.testi9').owlCarousel({
+  loop: true,
+  margin: 30,
+  nav: false,
+  dots: true,
+  autoplay: true,
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1
+
+    },
+    1650: {
+      items: 1
+    }
+  }
+})
+
 /* const mobNav = document.querySelector(".navigation");
 
 window.onscroll = function(){
